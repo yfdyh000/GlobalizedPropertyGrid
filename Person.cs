736a3152b4ce01b6a5c72caf9e1731a27f9b420a
YFdyh000 <yfdyh000@gmail.com>
2022-05-23 06:44:20 +0800

    init
    credit:
    https://www.codeproject.com/Articles/2138/Globalized-property-grid
    Globalized Property Grid
    Gerd Klevesaat
    17 Apr 2002

---
+++ Person.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace GlobalizedPropertyGrid
+{
+
+	/// <summary>
+	/// Person is the test class defining two properties: first name and last name .
+	/// By deriving from GlobalizedObject the displaying of property names are language aware.
+	/// GlobalizedObject implements the interface ICustomTypeDescriptor. 
+	/// </summary>
+	public class Person : GlobalizedObject
+	{
+		private string firstName = "";
+		private string lastName = "";
+		private int age = 0;
+
+		public Person() {}
+
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+
+		// Uncomment the next line to see the attribute in action: 
+		// [GlobalizedProperty("Surname",Description="ADescription",Table="GlobalizedPropertyGrid.SpecialStringTable")]
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+	}
+}
+
